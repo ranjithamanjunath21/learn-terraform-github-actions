29a0690f703839d4e4acdecb7bbaa52e72cd9267
Ranjitha Manjunath <140109301+ranjithamanjunath21@users.noreply.github.com>
2023-07-25 15:33:07 -0400

    Revert "Pointed TFC to backednd" (#2)

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos03"
+    organization = "REPLACE_ME"
 
     workspaces {
-      name = "Demo-GitHub-Actions"
+      name = "REPLACE_ME"
     }
   }
 }
